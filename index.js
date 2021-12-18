2088c0251f608aea8d6b46fd4165ceb42c75d1a6
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-12-18 15:42:47 +0530

    SETUP_RAZORPAY

--- index.js
+++ index.js
@@ -8,22 +8,31 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.post("/order", (req, res) => {
+app.post("/order", async (req, res) => {
   const amount = req.body.amount;
+
   var instance = new Razorpay({
     key_id: "YOUR_KEY_ID",
     key_secret: "YOUR_SECRET",
   });
 
-  instance.orders.create({
-    amount: 50000,
+  var options = {
+    amount: amount *100,
     currency: "INR",
-    receipt: "receipt#1",
-    notes: {
-      key1: "value3",
-      key2: "value2",
-    },
+    receipt: "order_rcptid_11",
+  };
+
+  const myOrder = await instance.orders.create(options);
+
+  res.status(201).json({
+      success: true,
+      amount: amount,
+      order: myOrder,
   });
+
+//   instance.orders.create(options, function(err,order){
+//       console.log(order);
+//   });
 });
 
 app.listen(4000, () => {
